209321b663f67612270f487c3359496fd36889ab
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-26 13:51:29 -0300

    Rework Descriptor methods and add get_columns method

--- gladeparser/columns.py
+++ gladeparser/columns.py
@@ -1,13 +1,13 @@
 import os
 from enum import Enum
+from typing import Union, List
 
 import numpy as np
 import pandas as pd
 
-dirname = os.getcwd()
-column_filepath = os.path.join(dirname, "columns.csv")
+COLUMNS_FILENAME = "columns.csv"
 
-_dtypes = {
+DTYPES = {
     "ID": int,
     "Catalog ID": str,
     "Object type flag": str,
@@ -17,6 +17,11 @@ _dtypes = {
     "Mass": np.float64,
     "Merger rate": np.float64,
 }
+
+
+def get_column_filepath() -> str:
+    dirname = os.getcwd()
+    return os.path.join(dirname, COLUMNS_FILENAME)
 
 
 class Group(str, Enum):
@@ -30,50 +35,65 @@ class Group(str, Enum):
     MERGER_RATE = "Merger rate"
 
     def dtype(self):
-        return _dtypes[self.name]
+        return DTYPES[self.name]
+
+    @classmethod
+    def values(cls):
+        return list(map(lambda g: g.value, cls))
 
 
 class GLADEDescriptor:
     def __init__(self):
+        column_filepath = get_column_filepath()
         self._columns = pd.read_csv(column_filepath, index_col="Column ID")
 
     def __str__(self):
         return str(self._columns)
 
     @property
-    def groups(self):
+    def groups(self) -> List[str]:
         return list(dict.fromkeys(self._columns["Group"]))
 
     @property
+    def names(self) -> List[str]:
+        return self._columns["Column Name"].to_list()
+
+    @property
     def column_dtypes(self):
-        dtype_list = self._columns["Group"].map(_dtypes)
-        return dict(zip(self._names(), dtype_list))
+        dtype_list = self._columns["Group"].map(DTYPES)
+        return dict(zip(self.names, dtype_list))
 
-    def _names(self, indices=None):
-        names = self._columns["Column Name"]
-        filtered = names[indices] if indices is not None else names[:]
-        return filtered.to_list()
+    def _index_to_name(self, indices: List[int]) -> List[str]:
+        return self._columns["Column Name"][indices].to_list()
 
-    def _get_index(self, column):
+    def _parse_column(self, column: Union[int, str]) -> List[int]:
         if isinstance(column, int):
             return [column]
         if not isinstance(column, str):
-            raise ValueError
+            raise ValueError("column argument should be int or str")
 
-        try:
-            Group(column)
-            query = f'Group == "{column}"'
-        except ValueError:
-            query = f'`Column Name` == "{column}"'
+        query = (
+            f'Group == "{column}"'
+            if column in Group.values()
+            else f'`Column Name` == "{column}"'
+        )
         return self._columns.query(query).index.to_list()
 
-    def get_columns(self, *args, names=False):
-        columns = []
+    def _columns_to_indices(self, *args: Union[int, str]) -> List[int]:
+        columns_indices = []
         for arg in args:
-            columns += self._get_index(arg)
+            columns_indices += self._parse_column(arg)
         # Normalize
-        if len(columns) == 0:
-            indices = None
-        else:
-            indices = sorted(list(set(columns)))
-        return self._names(indices) if names else indices
+        return sorted(list(set(columns_indices)))
+
+    def get_columns(self, *args: Union[int, str]) -> List[str]:
+        columns_indices = self._columns_to_indices(*args)
+        return (
+            self._index_to_name(columns_indices)
+            if len(columns_indices) > 0
+            else self.names
+        )
+
+
+def get_columns(*args: Union[int, str]) -> List[str]:
+    return GLADEDescriptor().get_columns(*args)
