70ac1f94ad6449dde9941557f077ce8695d4ad81
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-06-26 13:52:33 -0300

    Add new objects to top-level import

--- gladeparser/__init__.py
+++ gladeparser/__init__.py
@@ -1,3 +1,4 @@
 from .parser import to_df, to_hdf5
+from .columns import GLADEDescriptor, get_columns
 
-__all__ = ("to_df", "to_hdf5")
+__all__ = ("to_df", "to_hdf5", "GLADEDescriptor", "get_columns")
