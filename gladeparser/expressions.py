a8ba699d2c4066fab61a15cf1eb00cd237b5a586
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-07-25 15:16:19 -0300

    Add custom polars filters

---
+++ gladeparser/expressions.py
@@ -0,0 +1,17 @@
+import polars as pl
+
+
+def filter_by_valid_redshifts(zmin: float = 0.0):
+    return (pl.col("z_cmb") > zmin, pl.col("z_cmb").is_not_null())
+
+
+def filter_by_catalog_name(catalog_name: str):
+    return (pl.col(catalog_name).is_not_null(),)
+
+
+PGC = filter_by_catalog_name("PGC no")
+GWGC = filter_by_catalog_name("GWGC name")
+WISExSCOS = filter_by_catalog_name("WISExSCOS name")
+TWOMASS = filter_by_catalog_name("2MASS name")
+HYPERLEDA = filter_by_catalog_name("HyperLEDA name")
+SDSS_DR16Q = filter_by_catalog_name("SDSS-DR16Q name")
